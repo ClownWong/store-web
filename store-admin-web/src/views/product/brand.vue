55836067a302477e54e677269f835b0ffa31903e
ClownWong <123>
2019-10-11 22:59:36 +0800

    web

---
+++ store-admin-web/src/views/product/brand.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>品牌管理</div>
+</template>
+
+<script>
+
+
+    export default {
+        data(){
+            return{ message:"品牌管理"}
+        }
+
+    }
+
+</script>
+
+<style scoped>
+</style>
